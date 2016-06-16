82d7b6606cd10d4d7067dcb8f7dfffd728c53c34
LongHorn-C <lhkind@gmail.com>
2016-06-16 18:15:19 +0800

    solve Hamilton path with back-track framework.

--- bbb_Hamilton.c
+++ bbb_Hamilton.c
@@ -4,93 +4,103 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "bbb.h"
+#include "g.h"
 #include "common.h"
 
 //to do :deal with the last node return to the first.
-static int _pass(List *a_p_p,PData a_i,PData ctx)
-	G *g =(G *)ctx;
-	int v=((G_e *)((ListNode *)a_i)->data)->iEnd;
-	
-	if(a_p_p->n < g->n){
-		int vn;
-		ListNode *n= a_p_p->head;
-		while(n){
-			vn = ((G_e *)n->data)->iEnd;
-			if ( a_p_p->n < g->n+1 && vn ==v)
-				return 0;
-			
-			n=n->next;
-		}	
-	}
-	else if( a_p_p->n == g->n ){
-		int v0 = ((G_e *)a_p_p->head->data)->iEnd;
+static int _pass(List *path,PData a_i,PData ctx){
+	G *g = ctx;
+	ListNode *edge_node = a_i;
+	G_e *edge = edge_node->data;
+	int v = edge->iEnd;
+
+	if( path->n == g->n ){
+		edge_node = path->head->data;
+		edge = edge_node->data;
+		int v0 = edge->iEnd;
+
 		if (v0 ==v )
 			return 2;
 	}
-	
+	else if (path->n > g->n)
+        return 0;
+	else{
+		ListNode *n= path->head;
+		while(n){
+			edge_node = n->data;
+			edge = edge_node->data;
+			int vp = edge->iEnd;
+			if (vp == v)
+				return 0;
+
+			n = n->next;
+		}
+	}
 	return 1;
 }
 
-/*
-static int _is_answer(List *a_p_p,PData ctx){
-	G *g =(G *)ctx;
-	return (a_p_p->n == g->n+1);
-}
-*/
 
-static int _first_child(List *a_p_p,PData a_i,PData ctx){
-	G *g =(G *)ctx;
-	G_e *e,*e1;
-	ListNode n = a_p_p->tail;
-	e = (G_e *) n->data;
-	int u = e->iEnd;
-	e1 = g->adj[u]->head;
-	Answer_node ** ppa= (Answer_node **)a_i;
-	*ppa = el;
-	result =  (el != NULL);
-	
-}	
+static int _get_first_child(List *path,PData a_i,PData ctx){
+	G *g = ctx;
 
-static int _next_sibling(List *a_p_p,PData a_i,PData ctx){
-	Answer_node ** ppa= (Answer_node **)a_i;
-	*ppa = (*ppa)->next;
-	return *a_i != NULL;	
+	ListNode *ans_tail_node = path->tail;
+	ListNode *node = ans_tail_node->data;
+	G_e *edge = node->data;
+	int u = edge->iEnd;
+
+	node = g->v_adj[u]->head;
+
+	if (node)
+		*(ListNode *)a_i = *node;
+
+	return  (node != NULL);
+
 }
 
-static _print_result(List *a,PData ctx){
-	G *g= (G *)ctx;
-	ListNode *n= a_p_p->head;
-	while (n){
-		ListNode *a = (ListNode *)n->data;
-		G_e *e= (G_e *)a->data;
-		printf("%s -> ",g->v[e->iEnd].name);
-		n = n->next;
-	}
+static int _get_next_sibling(List *path,PData a_i,PData ctx){
+	ListNode * node = a_i;
+	node = node->next;
+
+	if (node)
+		*(ListNode *)a_i = *node;
+
+	return node != NULL;
+}
+
+static int _print_node(ListNode *n,PData ctx){
+	G *g= ctx;
+	ListNode *edge_node = (ListNode *)n->data;
+	G_e *edge= (G_e *)edge_node->data;
+	printf("%s -> ",g->v[edge->iEnd].name);
 }
 
 
 int Hamilton(G *g){
 	Bt_callbacks cb;
-	cb.pass =_pass;
-	//cb.is_answer =_is_answer;
-	cb.first_child =_first_child;
-	cb.next_sibling =_next_sibling;
 
-	
+	cb.pass =_pass;
+	cb.get_first_child =_get_first_child;
+	cb.get_next_sibling =_get_next_sibling;
+
+
     List * ans = newList();
 	ListNode a;
 	a.next =NULL;
 	G_e e;
+	e.iBegin = 0;
 	e.iEnd = 0;
-	a->data = &e;
-	list_append(ans,(PData) &a);
+	e.weight = 0;
+	e.data = (PData)-1;
+	a.data = &e;
+	list_append(ans,&a);
 
-	ListNode **ppn;
-	_first_child(ans,ppn,(PData)g);
-	
-	if (back_track(ans,*ppn,&cb,(PData)g))
-		_print_result(ans,g);
-	
+	ListNode node;
+	_get_first_child(ans,&node,g);
+
+	if (back_track(ans,&node,sizeof(ListNode),&cb,g))
+		list_foreach(ans,_print_node,g);
+	freeList(ans,0);
+
 }
 
 void main(){
@@ -101,20 +111,20 @@ void main(){
 	assignVertex(g,3,"d");
 	assignVertex(g,4,"e");
 	assignVertex(g,5,"f");
-	
+
 	addEdge_NoDirection(g,0,1,1);
 	addEdge_NoDirection(g,0,2,1);
-	addEdge_NoDirection(g,0,3,1);	
+	addEdge_NoDirection(g,0,3,1);
 
-	addEdge_NoDirection(g,1,2,1);	
-	addEdge_NoDirection(g,1,5,1);	
+	addEdge_NoDirection(g,1,2,1);
+	addEdge_NoDirection(g,1,5,1);
 
-	addEdge_NoDirection(g,2,3,1);	
+	addEdge_NoDirection(g,2,3,1);
 	addEdge_NoDirection(g,2,4,1);
-	
-	addEdge_NoDirection(g,3,4,1);	
-	
-	addEdge_NoDirection(g,4,5,1);	
 
-	Hamilton(g);	
+	addEdge_NoDirection(g,3,4,1);
+
+	addEdge_NoDirection(g,4,5,1);
+
+	Hamilton(g);
 }
